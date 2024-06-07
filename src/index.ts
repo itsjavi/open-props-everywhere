bf71aeb3e2c059c8cdbdac8c123ed6ae0e0de7ad
Javi Aguilar <122741+itsjavi@users.noreply.github.com>
2024-06-07 02:23:19 +0200

    chore: add License and improve JSR config

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
-/**
- * Entry point for the transformed Open Props tokens (used by the framework adapters), and other utilities.
- *
- * @module
- */
-
 export * from './tokens'
+export type { TokenConfig } from './types'
+

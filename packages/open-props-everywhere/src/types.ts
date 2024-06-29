1ae762ece518fd977ac8d506bcbb8cb4dead4cad
Javi Aguilar <122741+itsjavi@users.noreply.github.com>
2024-06-29 20:55:52 +0200

    refactor: reuse types

--- packages/open-props-everywhere/src/types.ts
+++ packages/open-props-everywhere/src/types.ts
@@ -4,15 +4,7 @@ export type PandaRecursiveToken<T = string> = Recursive<Token<T>>
 
 export type PandaRecursiveSemanticToken<T = string> = Recursive<SemanticToken<T>>
 
-export type ColorGeneratorConfigMap = Record<
-  string,
-  {
-    hue: string | number
-    luminanceShift?: number
-    chromaShift?: number
-    isGray?: boolean
-  }
->
+export type ColorGeneratorConfigMap = Record<string, ColorGeneratorConfig>
 
 export type ColorGeneratorConfig = {
   hue: string | number
